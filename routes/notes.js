58b95bce3fc75b9723443a2005bca0dd97e8ce77
Chandrika Sista <cguntur@gmail.com>
2024-04-25 18:52:49 -0700

    Got the api's working for adding and deleting the notes

---
+++ routes/notes.js
@@ -0,0 +1,51 @@
+const notes = require('express').Router();
+const uuid = require('../helpers/uuid');
+
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes test`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+//POST route for a new note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const {title, text} = req.body;
+
+  if(req.body){
+    const newNote = {
+      id: uuid(),
+      title,
+      text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  }else {
+    res.errored('Error in adding a note');
+  }
+});
+
+//DELETE route
+notes.delete('/:id', (req, res) => {
+  console.info(`${req.method} request received for note`);
+  const noteId = req.params.id;
+  console.info(`Request received to delete Item ${noteId}`);
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter(note => note.id != noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted`);
+    });
+});
+
+  module.exports = notes;
---
+++ server.js
@@ -0,0 +1,28 @@
+// Import express package
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+
+const PORT = 3001;
+
+// Initialize our app variable by setting it to the value of express()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html')),
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Note taking app listening at http://localhost:${PORT}`)
+);
