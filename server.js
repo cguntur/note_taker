b72841c5d30bb8e5aec66fd6689926ed0bb2b2f9
Chandrika Sista <cguntur@gmail.com>
2024-04-26 17:06:56 -0700

    Updated the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
 
-const PORT = 3001;
+const PORT = process.env['PORT'] || 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
